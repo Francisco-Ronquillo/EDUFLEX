1de34292e42b553e97f368fda623da36de50d9a0
Francisco-Ronquillo <fronquilloj@unemi.edu.ec>
2025-06-06 21:33:51 -0500

    Agregacion de url de la IA

--- NIÑO/urls.py
+++ NIÑO/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'niño'
 urlpatterns = [
 path('dashboarKid/',DashboardKid.as_view(),name='dashboardKid'),
-path('video_feed/', views.video_feed, name='video_feed')
+
 
 ]
--- NIÑO/views.py
+++ NIÑO/views.py
@@ -9,5 +9,4 @@ class DashboardKid(TemplateView):
         if 'nino_id' not in request.session:
             return redirect('accounts:login')
         return super().dispatch(request, *args, **kwargs)
-def video_feed(request):
-    return StreamingHttpResponse(gen_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
+
